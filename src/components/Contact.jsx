b189fcb61696b29a1d2cd6026266c9e46b58e36c
robo311 <rhornik311@gmail.com>
2023-02-15 19:08:52 +0100

    Changed font weight and fixed width of elements

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,7 +28,7 @@ function Contact() {
     <section id='contact' className='mb-8'>
         <div className='w-full flex flex-col items-center mb-[50px]'>
             <h1 className='section-heading'>03. <span className='font-semibold'>Contact</span></h1>
-            <p className='text-center font-medium text-[20px] max-w-[700px]'>Do you have question or need help with anything? Don't hesitate and contact me! I'll try to get back to you as soon as possible.</p>
+            <p className='text-center text-[20px] max-w-[700px]'>Do you have question or need help with anything? Don't hesitate and contact me! I'll try to get back to you as soon as possible.</p>
         </div>
         <div>
             <form ref={form} onSubmit={sendEmail} className='flex flex-col gap-8 items-center'>
@@ -44,7 +44,7 @@ function Contact() {
                 </div>
                 <div className='flex flex-col gap-2'>
                     <label className='font-medium text-[18px]'>Your question</label>
-                    <textarea required id='message' name="message" className='md:w-[580px] h-[150px]'></textarea>
+                    <textarea required id='message' name="message" className='md:w-[575px] w-[240px] h-[150px]'></textarea>
                 </div>
                 <input type="submit" value="Send" className='outline outline-1 hover:outline-2 outline-[#4AFFA8] py-2 px-6 rounded-lg mt-4 transform hover:translate-y-[-2px] transition-transform duration-150 cursor-pointer'/>
             </form>
